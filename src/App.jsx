45f4df6952a13204b1b58f0b9edaa7d43e6c3f7f
shenmadu <tashen.madu@gmail.com>
2024-02-29 12:23:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Register from './components/Register'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className="container">
+      <div className="row">
+       <Register/>
+      </div>
+     </div>
+       
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Register.jsx
@@ -0,0 +1,72 @@
+import { useForm} from "react-hook-form"
+import Swal from "sweetalert2";
+
+export default function Register() {    
+    
+    const { register, handleSubmit, formState:{errors} } = useForm()
+  
+    const submit= (data)=>{
+        console.log(data);
+        fetch("http://localhost:8080/student",{
+            method:'POST',
+            headers:{
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+        .then(responce=> responce.json())
+        .then(data=>{
+            console.log(data);            
+            Swal.fire({
+                title: "Register Sucess!",
+                text: "You Register Succesful!",
+                icon: "success"
+              });
+        })
+        
+    }
+
+
+
+
+    return (
+        <div>
+            <div className="container col-xl-10 col-xxl-8 px-4 py-5">
+                <div className="row align-items-center g-lg-5 py-5">
+                    <div className="col-lg-7 text-center text-lg-start">
+                        <h1 className="display-4 fw-bold lh-1 text-body-emphasis mb-3">Register</h1>
+                        <p className="col-lg-10 fs-4">register stuent y entering details</p>
+                    </div>
+                    <div className="col-md-10 mx-auto col-lg-5">
+                        <form className="p-4 p-md-5 border rounded-3 bg-body-tertiary">
+                            <div className="form-floating mb-3">
+                                <input {...register("id", { required: true })} type="text" className="form-control" id="floatingInput" placeholder="id" />
+                                <label for="floatingInput">Id</label>
+                                {errors.id && <span>This field is required</span>}
+                            </div>
+                            <div className="form-floating mb-3">
+                                <input {...register("firstName",{ required: true })} type="text" className="form-control" id="floatingInput1" placeholder="First name" />
+                                <label for="floatingInput1">First Name</label>
+                                {errors.firstName && <span>This field is required</span>}
+                            </div>
+                            <div className="form-floating mb-3">
+                                <input {...register("lastName",{ required: true })} type="text" className="form-control" id="floatingInput2" placeholder="Last name" />
+                                <label for="floatingInput2">Last Name</label>
+                                {errors.lastName && <span>This field is required</span>}
+                            </div>
+                            <div className="form-floating mb-3">
+                                <input {...register("contactNumber",{ required: true })} type="text" className="form-control" id="floatingPassword" placeholder="Contact" />
+                                <label for="floatingPassword">Contact</label>
+                                {errors.contactNumber && <span>This field is required</span>}
+                            </div>
+                           
+                            <button onClick={handleSubmit(submit)} className="w-100 btn btn-lg btn-primary" type="submit">Register</button>
+
+                            
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
